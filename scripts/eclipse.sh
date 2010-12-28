f1064bee5db32034a6a24e18bfd8c0f157e75fb6
Michael Zoech <michi.zoech@gmail.com>
2010-12-28 17:48:36 +0100

    eclipse.sh should fail if sub-step fails

--- scripts/eclipse.sh
+++ scripts/eclipse.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-mvn -Pmaven clean
-mvn -Peclipse clean eclipse:clean eclipse:eclipse
-sed -i '/JRE_CONTAINER/d' app/.classpath device-tests/.classpath
+mvn -Pmaven clean || exit 1
+mvn -Peclipse clean eclipse:clean eclipse:eclipse || exit 1
+sed -i '/JRE_CONTAINER/d' app/.classpath device-tests/.classpath || exit 1
